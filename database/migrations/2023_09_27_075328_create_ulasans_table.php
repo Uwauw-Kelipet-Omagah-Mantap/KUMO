7cd2060090cfbdb4ca272d02ad02342a8e7fb80e
farizauliaazka <azka8794@gmail.com>
2023-09-27 15:07:48 +0700

    feat(migration): Ulasan

---
+++ database/migrations/2023_09_27_075328_create_ulasans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ulasan', function (Blueprint $table) {
+            $table->id('id_ulasan');
+            $table->unsignedBigInteger('id_mobil');
+            $table->unsignedBigInteger('id_pelanggan');
+            $table->text('ulasan');
+            $table->unsignedTinyInteger('rating');
+            //FK id_mobil
+            $table->foreign('id_mobil')->references('id_mobil')->on('mobil')
+                    ->cascadeOnDelete()->cascadeOnUpdate();
+            //FK id_pelanggan
+            $table->foreign('id_pelanggan')->references('id_pelanggan')->on('pelanggan')
+                    ->cascadeOnDelete()->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ulasan');
+    }
+};
